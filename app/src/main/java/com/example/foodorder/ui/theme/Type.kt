5779d2635efb9e984196b92c55c6b20e62f594ce
vlad-bstrv <keocera@icloud.com>
2024-06-02 18:33:17 +0300

    start

--- app/src/main/java/com/example/foodorder/ui/theme/Type.kt
+++ app/src/main/java/com/example/foodorder/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.foodorder.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.foodorder.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -32,3 +34,5 @@ val Typography = Typography(
     )
     */
 )
+
+val pacificoFont = FontFamily(Font(R.font.pacifico))
